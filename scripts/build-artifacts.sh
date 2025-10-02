40d9e3704288c9c2f55aef48a5b5e19ab413bb37
VictorPaiu <vpaiu@amazon.com>
2025-10-02 09:44:53 +0200

    Added ignored errors patching for the build artifacts

--- scripts/build-artifacts.sh
+++ scripts/build-artifacts.sh
@@ -6,6 +6,40 @@ get_mem_available_kb() {
     grep MemAvailable /proc/meminfo | awk '{print $2}'
 }
 
+copy_ignored_errors() {
+    local build_target_base="$1"
+    local target="$2"
+    
+    local present_working_dir="$(pwd)"
+    local config_file="$present_working_dir/configuration/$target.json"
+    if [ -f "$config_file" ]; then
+        local ignored_errors_path=$(jq -r '.ignoredErrors.path' "$config_file")
+        local ignored_errors_file="$present_working_dir/$ignored_errors_path/common-ignored-errors.json"
+        
+        if [ -f "$ignored_errors_file" ]; then
+            local build_dir="$(pwd)/$build_target_base/out/vs/editor/common/errors"
+            
+            echo "Copying ignored errors to $build_dir"
+            
+            # Strip comments and parse JSON
+            local clean_json=$(sed 's|//.*$||g' "$ignored_errors_file" | sed '/\/\*/,/\*\//d')
+            local string_count=$(echo "$clean_json" | jq '.stringPatterns | length')
+            local regex_count=$(echo "$clean_json" | jq '.regexPatterns | length')
+            
+            echo "Number of string ignored errors: $string_count"
+            echo "Number of regex ignored errors: $regex_count"
+            echo "Writing ignored errors to $build_dir"
+            
+            mkdir -p "$build_dir"
+            echo "$clean_json" | jq '.' > "$build_dir/ignored-errors.json"
+        else
+            echo "Ignored errors file not found: $ignored_errors_file"
+        fi
+    else
+        echo "Config file not found: $config_file"
+    fi
+}
+
 build() {
     local code_oss_build_target_base="$1"
     
@@ -29,6 +63,7 @@ build() {
     env \
         NODE_OPTIONS="--max-old-space-size=${max_space_size_mb}" \
         npm run gulp "$build_target"
+    cd ..
 }
 
 main() {
@@ -37,6 +72,7 @@ main() {
     local build_target_base=$("$(dirname "$0")/determine-build-target.sh" "$target")
     echo "Building for target: $build_target_base"
     build "$build_target_base"
+    copy_ignored_errors "$build_target_base" "$target"
 }
 
 main "$@"
